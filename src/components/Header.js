f37cb12b368088b67f9b5cbb885e302cdec604eb
George Smerin <gsmerin@gmail.com>
2019-03-24 14:47:24 +0000

    Import logo directly

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,20 @@
 import React from "react";
-import { Link, useStaticQuery } from "gatsby";
-import Img from "gatsby-image";
+import { Link } from "gatsby";
 import MainMenu from "./MainMenu";
-import MobileMenu from "./MobileMenu";
 import style from "./Header.module.scss";
-
+import logo from "../images/gs-logo.png";
 const Header = () => {
-  const { logo } = useStaticQuery(graphql`
-    query {
-      logo: file(relativePath: { eq: "gs-logo.png" }) {
-        childImageSharp {
-          fixed(width: 170, height: 170) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `);
-
   return (
     <header className={style.header}>
       <div className="container">
         <div className={style.headerContent}>
           <div className={style.headerLogo}>
             <Link to="/">
-              <Img
-                fixed={logo.childImageSharp.fixed}
+              <img
+                src={logo}
                 alt="George Smerin logo"
+                width="170"
+                height="170"
               />
             </Link>
           </div>
